902311b70bf6416cf4be18dfaeeda3ef1521ad97
yanli98 <yan.li.18@ucl.ac.uk>
2019-04-02 05:32:17 -0400

    Core Functionality 1, quiz points (for the questions set by your questions App) are loaded by clicking on a menu item

---
+++ quizFunctions.js
@@ -0,0 +1,90 @@
+// AJAX codes are adpated from Week5 practicals
+// Viewing data as GeoJSON codes are adapted from week 6&7 practicals
+
+// define global variables
+var xhrQuizPoints; // process AJAX request 
+var QuizLayer; // store quiz points
+
+function loadQuizPoints() 
+{
+	getPort();
+	alert("All Quiz Points will be loaded");
+	getQuizPoints();
+}
+
+// AJAX request to load quiz points
+function getQuizPoints(){
+	xhrQuizPoints = new XMLHttpRequest();
+	var url = "http://developer.cege.ucl.ac.uk:" + httpPortNumber + "/getQuizPoints/" + httpPortNumber;
+	xhrQuizPoints.open("GET", url, true);
+	xhrQuizPoints.onreadystatechange = processQuizPoints;
+	try {
+		xhrQuizPoints.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	}
+	catch (e) {
+		// this only works in internet explorer
+	}
+	xhrQuizPoints.send();
+}
+
+// AJAX response for loading quiz points
+function processQuizPoints(){
+	if (xhrQuizPoints.readState < 4){ 
+		console.log('Data Loading...'); // while waiting response from server
+	}
+	else if (xhrQuizPoints.readyState === 4) { // 4 = response from server completely loaded.
+		if (xhrQuizPoints.status > 199 && xhrQuizPoints.status < 300) {
+			// http status between 200 to 299 are all successful
+			var quizPoints = xhrQuizPoints.responseText;
+			loadQuizPointsLayer(quizPoints);
+		}
+	}
+}
+
+// Then, convert the received text (quiz points) from the server to JSON and add it to the map
+function loadQuizPointsLayer(quizPoints){
+	// convert the text received from the server to JSON 
+	var quizPointsJSON = JSON.parse(quizPoints);
+
+	// load the geoJSON layer 
+	QuizLayer = L.geoJSON(quizPointsJSON,
+	{
+		// use point to layer to create the points
+		pointToLayer: function(feature, latlng){
+			// in this case, we build an HTML DIV string
+			// using the values in the data
+			var htmlString = "<DIV id='popup'" + feature.properties.id + "><h5>" + feature.properties.question_title + "</h5>";
+			htmlString = htmlString + "<p>" + feature.properties.question_text + "</p>";
+			htmlString = htmlString + "<input type='radio' name='answer' id='" + feature.properties.id + " 1'/>" + feature.properties.answer_1 + "<br>";
+			htmlString = htmlString + "<input type='radio' name='answer' id='" + feature.properties.id + " 2'/>" + feature.properties.answer_2 + "<br>";
+			htmlString = htmlString + "<input type='radio' name='answer' id='" + feature.properties.id + " 3'/>" + feature.properties.answer_3 + "<br>";
+			htmlString = htmlString + "<input type='radio' name='answer' id='" + feature.properties.id + " 4'/>" + feature.properties.answer_4 + "<br><br />";
+			htmlString = htmlString + "<button onclick='checkAnswer(" + feature.properties.id + "); return false;'> Submit Answer</button>";
+			// now include a hidden element with the answer
+			// in this case the answer is alwasy the first choice
+			// for the assignment this will use feature.properties.correct_answer	
+			htmlString = htmlString + "<div id=answer" + feature.properties.id + " hidden>" + feature.properties.correct_answer + "</div>";
+			htmlString = htmlString + "</div>";
+			return L.marker(latlng, {icon: blueMarker}).bindPopup(htmlString);
+			}					
+	}).addTo(mymap);
+	
+	// zoom map so that all data are displayed
+	mymap.fitBounds(QuizLayer.getBounds());
+}
+
+// create markers
+var redMarker = L.AwesomeMarkers.icon({
+	icon: 'play',
+	markerColor: 'red'// create red marker
+});
+
+var blueMarker = L.AwesomeMarkers.icon({
+	icon: 'play',
+	markerColor: 'blue'// create blue marker
+});
+
+var greenMarker = L.AwesomeMarkers.icon({
+	icon: 'play',
+	markerColor: 'green'// create green marker
+});
