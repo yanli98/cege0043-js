bcbc953c94c8579ff70f582e2a11beeeeaf02fcf
yanli98 <yan.li.18@ucl.ac.uk>
2019-04-02 04:22:13 -0400

    Core Functionality 2, the lat/lng of the question point should be automatically entered when the user clicks on a map in the question app

--- uploadQuestion.js
+++ uploadQuestion.js
@@ -1,10 +1,32 @@
-// get question first and then upload data to database
+// create a custom popup when a point is clicked on the map
+var popup = L.popup(); 
+// define coordinate variables
+var latitude;
+var longitude;
+
+
+// get coordinates automatically when user clicks on a point
+function getCoords(e){
+	popup.setLatLng(e.latlng);
+	var coordStr = e.latlng.toString();
+	coordStr = coordStr.substr(7, coordStr.length - 1);
+	coordStr = coordStr.split(")")[0];
+	
+	latitude = coordStr.split(",")[0];
+	longitude = coordStr.split(",")[1];
+	
+	popup.setContent("Coordinates: " + latitude + " , " + longitude);
+	popup.openOn(mymap);
+	}
+
+// add the event detector to my map
+mymap.on('click', getCoords);
+
+// convert form to string
 function startQuestionUpload(){
 	// get coordinates
 	alert("Start upload data");
-	var latitude = document.getElementById("latitude").value;
-	var longitude = document.getElementById("longitude").value;
-	postString = "&latitude=" + latitude + "&longitude=" + longitude;
+	var postString = "&latitude=" + latitude + "&longitude=" + longitude;
 	
 	// get question title
 	var question_title = document.getElementById("question_title").value;
@@ -14,7 +36,7 @@ function startQuestionUpload(){
 	var question_text = document.getElementById("question_text").value;
 	postString = postString + "&question_text=" + question_text;
 	
-	// get possible answers
+	// get options
 	var answer_1 = document.getElementById("answer_1").value;
 	postString = postString + "&answer_1=" + answer_1;
 	var answer_2 = document.getElementById("answer_2").value;
@@ -24,7 +46,7 @@ function startQuestionUpload(){
 	var answer_4 = document.getElementById("answer_4").value;
 	postString = postString + "&answer_4=" + answer_4;
 	
-	// get the correct answer
+	// get correct answer
 	var correct_answer = document.getElementById("correct_answer").value;
 	postString = postString + "&correct_answer=" + correct_answer;	
 	
