0fe73b98d2b9ef5eed58933abc18827ba080231d
yanli98 <yan.li.18@ucl.ac.uk>
2019-04-02 22:26:00 -0400

    update menu options

--- rhMenu.js
+++ rhMenu.js
@@ -1,8 +1,8 @@
 // adapted from week 4 practical
-function menuClicked() {
-alert("You clicked the menu");
+function aboutClicked() {
+alert("This is a Location-Based Quiz App for UCL CEGE0043 assignment");
 }
 
-function addUCLLogo() {
-document.getElementById("ucllogo").innerHTML ="<img src='images/ucl.png'>"
+function contactClicked() {
+alert("Should you have any question, please contact Yan Li via email: yan.li.18@ucl.ac.uk");
 }
