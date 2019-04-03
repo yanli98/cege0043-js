bfbbbfbc0f6cb7626831065d65bf57cd12afaf49
yanli98 <yan.li.18@ucl.ac.uk>
2019-04-03 07:45:13 -0400

    update menu and startup

--- startup.js
+++ startup.js
@@ -1,9 +1,17 @@
-// functions to run as the page loaded
+// functions to run as the page loaded for quiz App
 function startup() {
     document.addEventListener('DOMContentLoaded', function () {
     loadW3HTML()
     getPort()
     trackLocation()
+    }, false);
+}
+
+// functions to run as the page loaded for question App
+function startupquestion() {
+    document.addEventListener('DOMContentLoaded', function () {
+    loadW3HTML()
+    getPort()
     }, false);
 }
 
